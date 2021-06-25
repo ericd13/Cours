b7fcc23802f24b499dbd9c98d79743bb2783d3a5
Éric DÉTREZ <mdp_ericd13@pop-os.localdomain>
2021-06-25 21:29:37 +0200

    Importation de sup commune

---
+++ ITC/Sup/Cours/Images/Machines/addition.s
@@ -0,0 +1,7 @@
+mov %rax, 3     ; place la valeur 3 (en base 10) 
+                ; dans le registre rax
+mov %rbx, 1     ; place la valeur 1 (en base 10) 
+                ; dans le registre rbx
+add %rax, %rbx  ; additionne les valeurs stockées 
+                ; dans rax et rbx de sorte que 
+                : le résultat soit dans rax
